413ca30520f4f55f2da9203d34bbb0b3ef396808
sonuTw <sonu@thoughtwin.com>
2022-09-16 12:59:59 +0530

    fix: code standars

--- pysuit/__init__.py
+++ pysuit/__init__.py
@@ -1,5 +1,6 @@
-__version__ = '0.1.0'
 from .main import PySuit
+
+__version__ = '0.1.0'
 
 
 __all__ = ("PySuit",)
--- pysuit/main.py
+++ pysuit/main.py
@@ -19,19 +19,27 @@ class PySuit:
             sortedDict = {}
             # Sorting dict by values
             if flag.lower() == "values":
-                sorted_dict = sorted(list(dictionary.values()),key=lambda x:(len(str(x)), str(x)))
+                sorted_dict = sorted(
+                    list(dictionary.values()),
+                    key=lambda x: (len(str(x)), str(x))
+                    )
                 for value in sorted_dict:
-                    key_result = [key for key in dictionary if dictionary[key] == value]
+                    key_result = [
+                        key for key in dictionary if dictionary[key] == value
+                    ]
                     sortedDict[key_result[0]] = value
             # Sorting dict by keys
             else:
-                sorted_dict = sorted(list(dictionary.keys()),key = lambda x:(len(str(x)), str(x)))
-                sortedDict = {key:dictionary[key] for key in sorted_dict}
+                sorted_dict = sorted(
+                    list(dictionary.keys()),
+                    key=lambda x: (len(str(x)), str(x))
+                    )
+                sortedDict = {key: dictionary[key] for key in sorted_dict}
 
             return sortedDict
 
         except Exception as e:
-            raise e         
+            raise e
 
 if __name__ == '__main__':
     pysuit = PySuit()
