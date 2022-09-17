2a9addd92290e5f29705e953d1450295bcdfb1ad
Deepak Patidar <deepakp@thoughtwin.com>
2022-09-17 19:33:36 +0530

    Flake8 Rules Updated

--- tests/test_py_suite.py
+++ tests/test_py_suite.py
@@ -2,11 +2,11 @@ from pysuit import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
 
 
 def test_add():
     a = 8
     b = 8
-    c = a+b
+    c = a + b
     assert c != 9
